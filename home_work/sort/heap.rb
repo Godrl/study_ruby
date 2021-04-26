4456f29aa613947f379da7c0ffdc5223f286e66a
hk <gusrl0515@gmail.com>
2021-04-26 13:49:24 +0900

    힙 소트 알고리즘 자식 노드 기준수정

--- home_work/sort/heap.rb
+++ home_work/sort/heap.rb
@@ -6,26 +6,24 @@ def heap_sort(array)
     heapify(arr, i, len)
   end
 
-  puts arr.inspect
   while len > 0
-    arr[0], arr[len] = arr[len], arr[0]
-    len -= 1
+    arr[0], arr[len] = arr[len], arr[0] # 가장 큰 값이 배열 첫번째 있으므로 바꿔준다.
+    len -= 1 # 가장 큰 값은 이미 정렬되었으므로 정렬할 배열의 숫자 -1
     heapify(arr, 0, len)
   end
-  puts array.inspect
 end
 
 def heapify(arr, parent, limit)
   root = arr[parent]
 
-  while (child = 2 * parent) <= limit do
-    if arr[child + 1].present?
+  while (child = (2 * parent) + 1) <= limit do # 왼쪽 자식노드부터 검사
+    if limit >= child + 1
       if child < limit && arr[child] < arr[child + 1]
-        child += 1
+        child += 1 # 오른쪽 자식노드
       end
     end
     break if root >= arr[child]
-    arr[parent], parent = arr[child], child
+    arr[parent], parent = arr[child], child # 자식노드가 자식노드를 갖고 있으면 그 자식노드에 대해 다시 검사를 해야하므로 값뿐만 아니라 index도 바꿔준다.
   end
   arr[parent] = root
 end
