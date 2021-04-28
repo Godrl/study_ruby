8bc7d4dba3bc80f02e43b41593ed7b1944822ade
hk <gusrl0515@gmail.com>
2021-04-28 10:30:57 +0900

    삽입 정렬 불필요한 주석 제거

--- home_work/sort/insertion.rb
+++ home_work/sort/insertion.rb
@@ -1,22 +1,17 @@
 def insertion_sort(array)
-  # 두번 째 값부터 반복
   for i in 1...(array.length)
     j = i
     while j > 0
-      #  이전 값이 더 크면
       if array[j - 1] > array[j]
-        # 교환
         array[j], array[j - 1] = array[j - 1], array[j]
       else
-        # 이전 값이 더 작으면 종료
-        break
+        break # 이전 값이 더 작으면 종료
       end
-      # 바로 전 값을 비교해줘야 하므로 1씩 감소시킨다.
-      j -= 1
+      j -= 1 # 바로 전 값을 비교해줘야 하므로 1씩 감소시킨다.
     end
   end
   puts "삽입 정렬 결과: #{array.inspect}"
 end
 
-arr = [1, 5, 10, 13, 20, 15, 100, 24, 90, 57]
+arr = [1, 10, 5, 13, 20, 15, 100, 24, 57, 90]
 insertion_sort(arr)
