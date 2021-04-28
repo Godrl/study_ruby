72acf884d169aff9020020680f796019f47333fd
hk <gusrl0515@gmail.com>
2021-04-28 11:01:29 +0900

    셸 정렬 간격 초기화 수정

--- home_work/sort/shell.rb
+++ home_work/sort/shell.rb
@@ -1,7 +1,6 @@
 def shell_sort(array)
   len = array.length
-  # 간격 초기화
-  gap = (len / 3) + 1
+  gap = (len / 3) + 1 # 간격 초기화
 
   # 간격이 1보다 작을때 까지 반복
   while gap >= 1
@@ -9,15 +8,14 @@ def shell_sort(array)
       j = i
       # gap 만큼 떨어진 곳에 값이 없을 때 까지 반복
       while j >= gap
-        # 앞에 있는 값이 더 크면
         if array[j - gap] > array[j]
           array[j], array[j - gap] = array[j - gap], array[j]
         end
         j -= gap
       end
     end
-    # 간격 초기화
     gap /= 3
+    gap += 1 if gap > 1 && gap.even? # 간격은 홀수일때 가장 효율이 좋다
   end
   puts "셸 정렬 결과: #{array.inspect}"
 end
